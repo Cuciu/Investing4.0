6fb96e8e8ddcdeb96ca0c905e5224a7efba9710e
Cuciu <alexandru.cuciurean@gmail.com>
2020-10-02 09:25:42 +0200

    Update App1.2.py

--- App1.2.py
+++ App1.2.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-from MarketWatch02 import MarketWatch_Value_Growth
+from MarketWatch02 import MarketWatch_Value_Growth, MarketWatch_Cash_Factor, MarketWatch_Profile
 from URLHandler import URLHandler
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -9,10 +9,10 @@ def Read(url):
     stockname = url.replace('https://www.marketwatch.com/investing/stock/', '')
     string_financials = BeautifulSoup(URLHandler(str(url) + '/financials'), "html.parser")
     financials = string_financials.find_all('tr', attrs={'class': 'mainRow'})
-    balancesheet = 0
-    #string_balancesheet = BeautifulSoup(URLHandler(str(url) + '/financials/balance-sheet'), 'html.parser')
-    #balancesheet = string_balancesheet.BeautifulSoup(URLHandler(str(url) + '/financials/balance-sheet'), 'html.parser')
-    r = [stockname, financials, balancesheet]
+    string_balancesheet = BeautifulSoup(URLHandler(str(url) + '/financials/balance-sheet'), 'html.parser')
+    balancesheet = string_balancesheet.find_all('tr', attrs={'class': {'totalRow', 'mainRow', 'partialSum'}})
+    string_profile = BeautifulSoup(URLHandler(str(url) + '/profile'), "html.parser")
+    r = [stockname, financials, balancesheet, string_profile]
     return r
 
 app = Flask(__name__)
@@ -32,7 +32,7 @@ def result():
         baseurl5 = request.form['Base_Link5']
         urls = [baseurl1, baseurl2, baseurl3, baseurl4, baseurl5]
 
-        Years = int(request.form['Years'])
+        #Years = int(request.form['Years'])
         NBShares = int(request.form['Number_Shares'])
         RealDiscountRate = float(request.form['RealDiscountRate'])
         AverageInflation = float(request.form['AverageInflation'])
@@ -45,6 +45,7 @@ def result():
             print(stockname)
             string_financials = Read(item)[1]
             string_balancesheet = Read(item)[2]
+            string_profile = Read(item)[3]
 
             # Net Income Growth
             r1 = '{}%'.format(MarketWatch_Value_Growth(string_financials, 'Consolidated Net Income')[0])
@@ -52,9 +53,12 @@ def result():
             # EPS Growth
             r3 = '{}%'.format(MarketWatch_Value_Growth(string_financials, ' EPS (Diluted)')[0])
             r4 = '{}%'.format(MarketWatch_Value_Growth(string_financials, ' EPS (Diluted)')[1])
-            r5 = 0 #Current Liabilities/Current Cash factor
-            r6 = 0 #Total Liabilities/Total Cash factor
-            r7 = 0 #Price per ernings
+            #Current Liabilities/Current Cash factor
+            r5 = '{}%'.format(MarketWatch_Cash_Factor(string_balancesheet, ' Total Current Liabilities', ' Cash & Short Term Investments'))
+            #Total Liabilities/Total Assets
+            r6 = '{}%'.format(MarketWatch_Cash_Factor(string_balancesheet, ' Total Liabilities', ' Total Assets'))
+            #Price per ernings
+            r7 = '{}'.format(MarketWatch_Profile(string_profile, 'P/E Current')[0]['data'])
 
             r = [stockname, r1, r2, r3, r4, r5, r6, r7]
             t.append(r)
